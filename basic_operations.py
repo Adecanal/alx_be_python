3a8d4a75fe47c7cf74110a0e483a72060777925f
Adecanal <canaladepro@gmail.com>
2025-05-22 14:31:11 +0100

    Completed basic arithmetic task

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Assign numbers to variables
+number1 = 10
+number2 = 5
+
+# Calculate and print results
+print("Addition of {} and {} is {}".format(number1, number2, number1 + number2))
+print("Subtraction of {} and {} is {}".format(number1, number2, number1 - number2))
+print("Multiplication of {} and {} is {}".format(number1, number2, number1 * number2))
