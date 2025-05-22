e54c1ba0e6728e733344e3d36e2ca877bc2306e4
Adecanal <canaladepro@gmail.com>
2025-05-22 15:00:57 +0100

    Add basic arithmetic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# Assign values to variables
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+# Print results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
